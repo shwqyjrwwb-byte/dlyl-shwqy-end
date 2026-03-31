64301e90695935a48829e1a60697ab4e3308fdee
shwqyjrwwb-byte <shwqyjrwwb@gmail.com>
2026-03-31 15:58:52 +0200

    Configure for Netlify deployment

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
   },
-  output: 'standalone',
+  // output: 'standalone', // not needed for Netlify
 }
 
 export default nextConfig
